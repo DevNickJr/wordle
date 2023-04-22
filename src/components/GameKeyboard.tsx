cf70f693f4bd7833a7aa3bef2b78f00df83c2563
Prince Codes <princecodes247@gmail.com>
2023-04-22 16:57:57 -0700

    feat: update GameKeyboard events

--- src/components/GameKeyboard.tsx
+++ src/components/GameKeyboard.tsx
@@ -1,45 +1,71 @@
-const GameKeyboard = () => {
-    return (
-        <div className="flex flex-col items-center gap-2">
-                <div className="keyboard_row">
-                    <button className="btn">Q</button>
-                    <button className="btn">W</button>
-                    <button className="btn">E</button>
-                    <button className="btn">R</button>
-                    <button className="btn">T</button>
-                    <button className="btn">Y</button>
-                    <button className="btn">U</button>
-                    <button className="btn">I</button>
-                    <button className="btn">O</button>
-                    <button className="btn">P</button>
+import React from "react";
+import clsx from "clsx";
 
-                </div>
-                <div className="keyboard_row">
-                    <button className="btn">A</button>
-                    <button className="btn">S</button>
-                    <button className="btn">D</button>
-                    <button className="btn">F</button>
-                    <button className="btn">G</button>
-                    <button className="btn">H</button>
-                    <button className="btn">J</button>
-                    <button className="btn">K</button>
-                    <button className="btn">L</button>
-                </div>
-                <div className="keyboard_row">
-                    <button className="btn">ENTER</button>
-                    <button className="btn">Z</button>
-                    <button className="btn">X</button>
-                    <button className="btn">C</button>
-                    <button className="btn">V</button>
-                    <button className="btn">B</button>
-                    <button className="btn">N</button>
-                    <button className="btn">M</button>
-                    <button className="btn">DEL</button>
-
-                </div>
-            </div>
-
-    );
+const GameKeyboard = ({
+  handleClick = (key) => {},
+  correctLetters = "",
+  wrongLetters = "",
+  almostCorrectLetters = "",
+}: {
+  handleClick: (key: string) => void;
+  correctLetters: string;
+  wrongLetters: string;
+  almostCorrectLetters: string;
+}) => {
+  const keyColor = (key: string) => {
+    if (wrongLetters.toUpperCase().includes(key)) {
+      return "bg-[#3a3a3c]";
+    } else if (almostCorrectLetters.toUpperCase().includes(key)) {
+      return "bg-[#b59f3b]";
+    } else if (correctLetters.toUpperCase().includes(key)) {
+      return "bg-[#538d4e]";
+    } else {
+      return "bg-[#818384]";
+    }
+  };
+  const keys = "QWERTYUIOPASDFGHJKLZXCVBNM".split("");
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <div className="keyboard_row">
+        {React.Children.toArray(
+          keys.slice(0, 10).map((key) => (
+            <button
+              onClick={() => handleClick(key)}
+              className={clsx("btn text-lg text-white", keyColor(key))}
+            >
+              {key}
+            </button>
+          )),
+        )}
+      </div>
+      <div className="keyboard_row">
+        {React.Children.toArray(
+          keys.slice(10, 19).map((key) => (
+            <button
+              onClick={() => handleClick(key)}
+              className={clsx("btn text-lg text-white", keyColor(key))}
+            >
+              {key}
+            </button>
+          )),
+        )}
+      </div>
+      <div className="keyboard_row">
+        <button className="btn text-sm">ENTER</button>
+        {React.Children.toArray(
+          keys.slice(19, 26).map((key) => (
+            <button
+              onClick={() => handleClick(key)}
+              className={clsx("btn text-lg text-white", keyColor(key))}
+            >
+              {key}
+            </button>
+          )),
+        )}
+        <button className={clsx("btn text-lg")}>DEL</button>
+      </div>
+    </div>
+  );
 };
 
-export default GameKeyboard
+export default GameKeyboard;
