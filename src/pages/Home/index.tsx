1c31802064358518c6622a6067e67c717cdba8ce
Prince Codes <princecodes247@gmail.com>
2023-04-22 17:41:46 -0700

    feat: create delete logic

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import GameKeyboard from "../../components/GameKeyboard.tsx";
-import GameBoxes from '../../components/GameBoxes.tsx';
-
-
+import GameBoxes from "../../components/GameBoxes.tsx";
 
 const Home = () => {
-  const [words, setWords] = useState<string[]>(["HOUSE", "Rosek", "jnj"])
-  const [word, setWord] = useState<number>(0)
+  const [words, setWords] = useState<string[]>(["HOUSE", "Rosek", "jnj"]);
+  const [word, setWord] = useState<number>(0);
+
+  const handleDelete = () => {
+    const newWords = [...words];
+    if (newWords[word].length === 0) return;
+    newWords[word] = newWords[word].slice(0, -1);
+    setWords(newWords);
+  };
+
   return (
-      <div className='pt-8 flex justify-center items-center flex-col gap-4'>
-        <h1 className='text-3xl font-extrabold mb-4'>Wordle</h1>
-        <GameBoxes word={word} words={words} />
-        <GameKeyboard />
-      </div>
-  )
-}
+    <div className="flex flex-col items-center justify-center gap-4 pt-8">
+      <h1 className="mb-4 text-3xl font-extrabold">Wordle</h1>
+      <GameBoxes word={word} words={words} />
+      <GameKeyboard handleDelete={handleDelete} />
+    </div>
+  );
+};
 
 export default Home;
