02c8a13402ef8a67c5e651c3f28e7df579014c6b
DevNickJr <nicholasduadei14@gmail.com>
2023-04-23 01:37:42 +0100

    updates for prince

--- src/components/GameBoxes.tsx
+++ src/components/GameBoxes.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 
 interface GameBoxesProps {
-    word: string
+    word: string,
+    words: string[]
 }
 
-const GameBoxes: React.FC<GameBoxesProps> = () => {
+const GameBoxes: React.FC<GameBoxesProps> = ({ word, words }) => {
+    console.log(word, words)
     return ( 
         <div className="flex flex-col gap-1">
-            {[0,1,2,3,4,5].map(num =>
-                <div key={num} className={`flex gap-1`}>
-                    {[0,1,2,3,4].map(num => <div key={num} className={`w-14 h-14 border-2 border-black/20 text-3xl flex justify-center items-center font-bold ${false && "border-black/40"} ${false && "bg-[#787c7e] text-white"} ${false && "bg-[#c9b458] text-white"}`}>F</div>)}
+            {words?.map(word => {
+                const wordArr = word.split("");
+               return <div key={word} className={`flex gap-1`}>
+                    {[0,1,2,3,4].map((num) => <div key={num} className={`w-14 h-14 border-2 border-black/20 text-3xl flex justify-center items-center font-bold ${false && "border-black/40"} ${false && "bg-[#787c7e] text-white"} ${false && "bg-[#c9b458] text-white"} capitalize`}>{wordArr[num]}</div>)}
+                </div>
+            }
+            )}
+            {[...Array(6 - words.length)]?.map(val =>
+                <div key={val} className={`flex gap-1`}>
+                    {[0,1,2,3,4].map(num => <div key={`${val} ${num}`} className={`w-14 h-14 border-2 border-black/20 text-3xl flex justify-center items-center font-bold ${false && "border-black/40"} ${false && "bg-[#787c7e] text-white"} ${false && "bg-[#c9b458] text-white"}`}></div>)}
                 </div>
             )}
         </div>
