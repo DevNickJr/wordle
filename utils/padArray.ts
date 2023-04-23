bc76ce3e23e569d7a67fb7300514c5fabf21b3ed
Prince Codes <princecodes247@gmail.com>
2023-04-22 17:58:54 -0700

    update padArray function

--- utils/padArray.ts
+++ utils/padArray.ts
@@ -1,9 +1,10 @@
 function padArray(arr: Array<string>, length = 6, filler = "") {
-    const result = arr.slice(0, length); // take the first six elements of the array
-    while (result.length < length) {     // add zeros until the array has six elements
-      result.push(filler);
-    }
-    return result;
+  const result = [...arr].slice(0, length); // take the first six elements of the array
+  while (result.length < length) {
+    // add zeros until the array has six elements
+    result.push(filler);
   }
+  return result;
+}
 
-  export default padArray
+export default padArray;
